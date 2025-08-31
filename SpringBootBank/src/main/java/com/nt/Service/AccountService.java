e0c391c983084c4c078ee6008de2d1a92b16f99c
Gnaneshwar Palusam <gnaneshwar1691997@gmail.com>
2025-08-30 21:07:43 -0500

    Update AccountService.java

--- SpringBootBank/src/main/java/com/nt/Service/AccountService.java
+++ SpringBootBank/src/main/java/com/nt/Service/AccountService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.nt.Entity.Accounts;
-import com.nt.Repo.Repo;
+import com.nt.Repo.Repo;;
 
 @Service
 public class AccountService {
