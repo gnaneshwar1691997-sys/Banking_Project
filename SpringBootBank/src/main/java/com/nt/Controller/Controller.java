883dd435e326f4275c531a4d3c3570771bfe31eb
Gnaneshwar Palusam <gnaneshwar1691997@gmail.com>
2025-09-15 13:39:28 -0500

    Update Controller.java

--- SpringBootBank/src/main/java/com/nt/Controller/Controller.java
+++ SpringBootBank/src/main/java/com/nt/Controller/Controller.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController
 
 import com.nt.Entity.Accounts;
 import com.nt.Service.AccountService;
